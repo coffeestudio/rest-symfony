9501695707b08e146e281c1ab779e7981f8d54d2
Art <art@shesha.ru>
2015-07-02 16:47:01 +0300

    IAdminViewDAO: new methods

--- RestAPIBundle/Handle/ByInterface/IAdminViewDAO.php
+++ RestAPIBundle/Handle/ByInterface/IAdminViewDAO.php
@@ -10,10 +10,31 @@ class IAdminViewDAO extends RestHandle
         return ['id' => 'getId', 'title' => 'getTitle'];
     }
 
+    public function get($accessor)
+    {
+        return function ($id) {
+            return $this->mkResult($this->getDAO()->get($id));
+        };
+    }
+
     public function getList($accessor)
     {
         return function () {
             return $this->mkResult($this->getDAO()->getList());
+        };
+    }
+
+    public function listViewFields($accessor)
+    {
+        return function () {
+            return $this->getDAO()->listViewFields();
+        };
+    }
+
+    public function editViewFields($accessor)
+    {
+        return function () {
+            return $this->getDAO()->editViewFields();
         };
     }
 }
